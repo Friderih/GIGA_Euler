25797ea658cf2587eadd144291355e872dec85fd
Matija Pučnik <pucnik.matija@gmail.com>
2025-01-21 18:55:32 +0100

    Added first exercise to main

---
+++ Source code of Exercises not integrated/EU0001_any_input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int multiples(int n){
+    return ((n*(999/n)*(999/n+1))/2);
+}
+
+int main(){
+    int limita;
+    std::cout<<multiples(3)+multiples(5)-multiples(15)<<std::endl;
+    return 0;
+}
