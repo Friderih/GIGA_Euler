63a9a79be8c0e92515ff6685717aa947724f6543
Matija Pučnik <pucnik.matija@gmail.com>
2025-01-22 21:26:49 +0100

    Added third exercise

---
+++ Source code of Exercises not integrated/EU0003.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+int prime_chack(int number){
+    int limit=(int)sqrt(number)+1;
+    for(int i=2;i<=limit;i++){
+        if(number%i==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(){
+    long long int stevilo=600851475143;
+    long long int koren=(long long int)sqrt(stevilo)+1;
+    for(int i=koren;i>=2;i--){
+        if(stevilo%i==0){
+            if(prime_chack(i)==1){
+                std::cout<<i<<std::endl;
+                return 0;
+            }
+        }
+    }
+    std::cout<<stevilo<<std::endl;
+    return 0;
+}
