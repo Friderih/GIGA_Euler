8b7cc71d3ea06835a725ad608063dcb0e258f9ba
Matija Pučnik <pucnik.matija@gmail.com>
2025-01-24 17:58:26 +0100

    Added a part of exercise 8

---
+++ Source code of Exercises not integrated/EU0008.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+#include <fstream>
+
+int main(){
+    std::ifstream inputFile("EU0008_input.txt");
+    std::ifstream outputFile("EU0008_output.txt");
+    std::queue<int> vrsta;
+    int zmnozek=0;
+    inputFile>>zmnozek;
+    vrsta.push(zmnozek);
+    for(int i=0;i<12;i++){
+        
+    }
+
+    return 0;
+}
